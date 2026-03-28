34ed94d9599b68b32d51cd197f0cea6593135f4f
Gavin <gavinlo6188@gmail.com>
2026-03-28 14:10:13 +0000

    random array (y = x * (+-5%))

--- Programming_3_lab/task_2.cpp
+++ Programming_3_lab/task_2.cpp
@@ -3,24 +3,32 @@
 #include "vector"
 #include "fstream"
 
-using namespace std;
 int main() {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(1, 100);
-    vector<vector<char>> map(3, vector<char>(99, 1));
-    ofstream outFile("output.txt");
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    
+    std::ofstream outFile("output.txt");
+    
+    using namespace std;
+    uniform_real_distribution<> dis(0.95, 1.05);
+    vector<vector<float>> map(3, vector<float>(99, 1));
+    
 
     if (outFile.is_open()) {
-        outFile << "Random numbers between 1 and 100:\n";
+        outFile << "Random numbers between n(0.95 and 1.05):\n";
         int i = 0;
         int j = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 99 && i == 0; j++) {
-                map[i][j] = dis(gen);
+                map[i][j] = (j + 1) * dis(gen);
+                map[i+1][j] = j + 1;
                 outFile << map[i][j] << " " ;
+                outFile << map[i+1][j] << "\n";
             }
         }
     }
+    
+
+    cout<<"hello"<<endl;
     outFile.close();
 }
