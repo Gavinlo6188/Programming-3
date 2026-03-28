5d74d90e58cb2f1ffc4065e5f5c2e4dab8f269ad
Gavin <gavinlo6188@gmail.com>
2026-03-28 23:11:55 +0000

    variables added

--- Programming_3_lab/task_2.cpp
+++ Programming_3_lab/task_2.cpp
@@ -13,6 +13,16 @@ int main() {
     uniform_real_distribution<> dis(0.95, 1.05);
     vector<vector<float>> map(3, vector<float>(99, 1));
     
+    float i_last = 0.0;
+    float j_last = 0.0;
+    float i_now = 0.0;
+    float j_now = 0.0;
+    float i_change_sum = 0.0;
+    float j_change_sum = 0.0;
+
+    // y = mx + c
+    float m = 2.0;
+    float c = 0.0;
 
     if (outFile.is_open()) {
         outFile << "Random numbers between n(0.95 and 1.05):\n";
@@ -20,8 +30,15 @@ int main() {
         int j = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 99 && i == 0; j++) {
-                map[i][j] = 2 * (j + 1) * dis(gen);
-                map[i+1][j] = (j + 1) * dis(gen);
+                j_now = m * (j + 1) * dis(gen) + c;
+                i_now = (j + 1) * dis(gen);
+                i_change_sum += i_now - i_last;
+                j_change_sum += j_now - j_last;
+
+                map[i][j] = i_now;
+                map[i+1][j] = j_now;
+                i_last = i_now;
+                j_last = j_now;
                 outFile << map[i][j] << " " ;
                 outFile << map[i+1][j] << "\n";
             }
